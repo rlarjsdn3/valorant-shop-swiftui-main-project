5818522714a85317a155e87fa72b363d5f8422db
Zizi_Kim <rlarjsdn3@naver.com>
2023-09-12 23:15:23 +0900

    [add]: ValorantShop 프로젝트 추가

---
+++ ValorantShop/ValorantShop/ValorantShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValorantShopApp.swift
+//  ValorantShop
+//
+//  Created by 김건우 on 2023/09/12.
+//
+
+import SwiftUI
+
+@main
+struct ValorantShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
