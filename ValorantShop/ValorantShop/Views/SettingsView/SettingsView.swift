e8585d942a399984a84ca84ae9bece659ae2ad56
Zizi_Kim <rlarjsdn3@naver.com>
2023-09-13 00:04:15 +0900

    [add]: SettingsView 파일 추가

---
+++ ValorantShop/ValorantShop/Views/SettingsView/SettingsView.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsView.swift
+//  ValorantShop
+//
+//  Created by 김건우 on 2023/09/13.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings View")
+            .font(.largeTitle)
+            .fontWeight(.black)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
