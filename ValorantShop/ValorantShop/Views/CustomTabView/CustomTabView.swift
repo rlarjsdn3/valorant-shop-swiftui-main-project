21fdc709bad87ae059ebb9ac72861967e4c14780
Zizi_Kim <rlarjsdn3@naver.com>
2023-09-12 23:39:11 +0900

    [add]: CustomTabView 파일 추가#1

---
+++ ValorantShop/ValorantShop/Views/CustomTabView/CustomTabView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTabView.swift
+//  ValorantShop
+//
+//  Created by 김건우 on 2023/09/12.
+//
+
+import SwiftUI
+
+struct CustomTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTabView()
+    }
+}
