4a7f366e6b84563478713c029028997ff4904967
Zizi_Kim <rlarjsdn3@naver.com>
2023-09-14 00:15:06 +0900

    [add]: Color+Extension 파일 추가

---
+++ ValorantShop/ValorantShop/Color+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Extension.swift
+//  ValorantShop
+//
+//  Created by 김건우 on 2023/09/14.
+//
+
+import SwiftUI
+
+extension Color {
+    static var valorantThemeColor: Color {
+        Color(red: 235.0 / 255.0, green: 86.0 / 255.0, blue: 91.0 / 255.0)
+    }
+}
