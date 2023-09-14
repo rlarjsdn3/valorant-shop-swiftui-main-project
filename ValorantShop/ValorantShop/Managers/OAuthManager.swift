c743d67fe1a389fff3e75850a892803c78394da9
Zizi_Kim <rlarjsdn3@naver.com>
2023-09-14 22:32:58 +0900

    [add]: OAuthManager 파일 추가 #4

---
+++ ValorantShop/ValorantShop/Managers/OAuthManager.swift
@@ -0,0 +1,20 @@
+//
+//  OAuthManager.swift
+//  ValorantShop
+//
+//  Created by 김건우 on 2023/09/14.
+//
+
+import Foundation
+
+final class OAuthManager {
+    
+    // MARK: - SINGLETON
+    static let shared = OAuthManager()
+    private init() { }
+    
+    // MARK: - FUNCTIONS
+    
+    
+    
+}
