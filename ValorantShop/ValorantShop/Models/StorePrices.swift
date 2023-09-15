4ee1542221cf402114b96c6bf94022f1167ed377
Zizi_Kim <rlarjsdn3@naver.com>
2023-09-15 23:57:16 +0900

    [feat]: StorePrices 모델 구현 #11

--- ValorantShop/ValorantShop/Models/StorePrices.swift
+++ ValorantShop/ValorantShop/Models/StorePrices.swift
@@ -8,5 +8,27 @@
 import RealmSwift
 
 final class StorePrices: Object, Codable {
+    @Persisted var offers: List<Offer>
     
+    enum Codingkeys: String, CodingKey {
+        case offers = "Offers"
+    }
+}
+
+final class Offer: Object, Codable {
+    @Persisted var offerID: String // 무기 스킨의 첫 번째 Level의 UUID값
+    @Persisted var cost: Cost
+    
+    enum CodingKeys: String, CodingKey {
+        case offerID = "OfferID"
+        case cost = "Cost"
+    }
+}
+
+final class Cost: Object, Codable {
+    @Persisted var vp: Int
+
+    enum CodingKeys: String, CodingKey {
+        case vp = "85ad13f7-3d1b-5128-9eb2-7cd8ee0b5741"
+    }
 }
