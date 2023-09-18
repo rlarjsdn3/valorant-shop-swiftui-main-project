31324f5f84d3bb3df0121e59d44882f74dcb1f47
Zizi_Kim <rlarjsdn3@naver.com>
2023-09-18 23:22:46 +0900

    [add]: MultifactorAuthView 파일 추가

---
+++ ValorantShop/ValorantShop/Views/LoginView/MultifactorAuthView.swift
@@ -0,0 +1,20 @@
+//
+//  MultifactorAuthView.swift
+//  ValorantShop
+//
+//  Created by 김건우 on 2023/09/18.
+//
+
+import SwiftUI
+
+struct MultifactorAuthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MultifactorAuthView_Previews: PreviewProvider {
+    static var previews: some View {
+        MultifactorAuthView()
+    }
+}
