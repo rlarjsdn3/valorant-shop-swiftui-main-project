6d35859d6a9e75ad4b03421d4cef162d8871fbea
Zizi_Kim <rlarjsdn3@naver.com>
2023-09-23 12:10:43 +0900

    [add]: 무기 스킨 등급 이미지와 색상 데이터 추가

--- ValorantShop/ValorantShop/Color+Extension.swift
+++ ValorantShop/ValorantShop/Color+Extension.swift
@@ -13,4 +13,24 @@ extension Color {
         Color(red: 235.0 / 255.0, green: 86.0 / 255.0, blue: 91.0 / 255.0)
     }
     
+    static var selectEdition: Color {
+        Color(red: 90.0 / 255.0, green: 159.0 / 255.0, blue: 226.0 / 255.0)
+    }
+    
+    static var deulxeEdition: Color {
+        Color(red: 0 / 255.0, green: 149.0 / 255.0, blue: 135.0 / 255.0)
+    }
+    
+    static var premiumEdition: Color {
+        Color(red: 209.0  / 255.0, green: 84.0 / 255.0, blue: 141.0 / 255.0)
+    }
+    
+    static var exclusiveEdition: Color {
+        Color(red: 245.0 / 255.0, green: 149.0 / 255.0, blue: 91.0 / 255.0)
+    }
+    
+    static var ultraEdition: Color {
+        Color(red: 250.0 / 255.0, green: 214.0 / 255.0, blue: 99.0 / 255.0)
+    }
+    
 }
