16bab213d1e86dfb8226ead66a9742eb57a00b9d
Zizi_Kim <rlarjsdn3@naver.com>
2023-10-28 21:55:59 +0900

    [add]: MainView에 주석 추가

--- ValorantShop/ValorantShop/Views/MainView/MainView.swift
+++ ValorantShop/ValorantShop/Views/MainView/MainView.swift
@@ -40,6 +40,15 @@ struct MainView: View {
             CustomTabView()
         }
         .onAppear {
+            // ✏️ onAppear에서는 (앱이 처음 실행되면)
+            // 사용자 ID, 재화 정보와 내스킨 정보를 불러오고,
+            // 토큰 만료 유무에 따라 서버 혹은 Realm에서 상점 정보를 불러옴.
+            
+            // ✏️ Timer에서는 (앱이 백그→포그 혹은 실행 중이라면)
+            // 상점 정보의 만료 유무에 따라 서버에서 상점 정보를 불러옴.
+            // Realm에서는 불러올 필요가 없는 이유는 메모리에서 해제된 상태가 아니기 때문임.
+            // (즉, Persisted 변수에 상점 정보가 유지되어 있음)
+            
             let taskQueue = TaskQueue()
             
             Task {
@@ -156,7 +165,6 @@ struct MainView: View {
             }
         }
         .onReceive(willEnterForegroundNotification) { _ in
-            print("WillEnterForeground: 발로란트 최신 데이터 확인하기")
             // 최신 버전의 데이터가 존재하는지 확인하기
             Task {
                 await viewModel.checkValorantVersion()
